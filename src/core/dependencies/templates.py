b0dc21da1ac75bf5f97ea6ef5a409acb5be92afb
i-walk-away <orchidfake999@yandex.ru>
2026-06-27 23:35:34 +0300

    minor docstring grammar fix

--- src/core/dependencies/templates.py
+++ src/core/dependencies/templates.py
@@ -3,7 +3,7 @@ from fastapi.templating import Jinja2Templates
 
 async def get_templates() -> Jinja2Templates:
     """
-    Constructs a Jinja2Templates instances need for
+    Constructs a Jinja2Templates instances needed for
     working with templates and returning HTML pages.
     """
     return Jinja2Templates(directory="templates")
